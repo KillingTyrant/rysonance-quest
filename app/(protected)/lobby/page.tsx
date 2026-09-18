7fd957dc379d354ed33bdd40e267f1412d1ae670
SalvatoreLa <salvatore.la.spata@gmail.com>
2026-09-18 20:02:59 +0200

    merge ric

--- app/(protected)/lobby/page.tsx
+++ app/(protected)/lobby/page.tsx
@@ -21,7 +21,7 @@ export default function LobbyPage() {
             Tutti i personaggi che hai creato con il wizard.
           </p>
         </div>
-        <Button asChild variant="ticket" className="w-52">
+        <Button variant="ticket">
           <Link href="/onboarding">Crea un personaggio</Link>
         </Button>
       </header>
